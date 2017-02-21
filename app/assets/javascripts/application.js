8d19bf1e585dafbfc86ec173e3e454a02527baae
Lunga Sizani <lunga@go2africa.com>
2017-02-21 19:21:36 +0200

    Added views for guests

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,18 +9,18 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//= require libs/jquery.min.js
+//= require jquery.min.js
 //= require jquery_ujs
-//= require libs/modernizr-2.6.2.min.js
-//= require libs/jquery.easing.1.3.js
-//= require libs/bootstrap.min.js
-//= require libs/owl.carousel.min.js
-//= require libs/jquery.stellar.min.js
-//= require libs/jquery.waypoints.min.js
-//= require libs/jquery.countTo.js
-//= require libs/main.js
-//= require libs/marquee.js
-//= require libs/respond.min-.js
+//= require modernizr-2.6.2.min.js
+//= require jquery.easing.1.3.js
+//= require bootstrap.min.js
+//= require owl.carousel.min.js
+//= require jquery.stellar.min.js
+//= require jquery.waypoints.min.js
+//= require jquery.countTo.js
+//= require main.js
+//= require marquee.js
+//= require respond.min-.js
 //= require_tree .
 
 function myFunction() {
