8296b87508a5f9d1c6ee7ece40f1649c03c64855
Lunga Sizani <lunga@go2africa.com>
2017-04-01 20:39:14 +0200

    Capify

--- Gemfile
+++ Gemfile
@@ -25,15 +25,10 @@ gem 'sidekiq-cron'
 gem 'responders'
 gem 'react-rails'
 
-# Use Capistrano for deployment
-# gem 'capistrano-rails', group: :development
 group :production do
   gem 'unicorn'
 end
 
-group :heroku do
-  gem 'puma'
-end
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
@@ -59,4 +54,5 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'capistrano-rails'
 end
--- app/services/money_transaction_service.rb
+++ app/services/money_transaction_service.rb
@@ -4,7 +4,7 @@ class MoneyTransactionService
     @money_request = money_request
   end
   
-  def make_transaction_for_withdrawal(donation, use_request=false, balance=0)
+  def make_withdrawal_transaction(donation, use_request=false, balance=0)
     MoneyTransaction.create(withdrawal_id: @money_request.id,
                             donation_id: donation.id,
                             amount: use_request ? balance : donation.balance,
@@ -19,16 +19,16 @@ class MoneyTransactionService
       return if balance <= 0
       
       if balance > donation.balance
-        make_transaction_for_withdrawal(donation)
+        make_withdrawal_transaction(donation)
         balance -= donation.balance
       else
-        make_transaction_for_withdrawal(donation, true, balance)
+        make_withdrawal_transaction(donation, true, balance)
         balance -= balance
       end
     end
   end
 
-  def make_transaction_for_donation(withdrawal, use_request=false, balance=0)
+  def make_donation_transaction(withdrawal, use_request=false, balance=0)
     MoneyTransaction.create(withdrawal_id: withdrawal.id,
                             donation_id: @money_request.id,
                             amount: use_request ? balance : withdrawal.balance,
@@ -42,12 +42,12 @@ class MoneyTransactionService
     
     withdrawals.each do |withdrawal|
       return if balance <= 0
-      pp "Balance: #{balance}"
+
       if balance > withdrawal.balance
-        make_transaction_for_donation(withdrawal)
+        make_donation_transaction(withdrawal)
         balance -= withdrawal.balance
       else
-        make_transaction_for_donation(withdrawal, true, balance)
+        make_donation_transaction(withdrawal, true, balance)
         balance -= balance
       end
     end
