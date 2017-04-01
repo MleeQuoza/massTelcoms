90ecda99536373ddd818b0d965f1b2e49be5e32d
Lunga Sizani <lunga@go2africa.com>
2017-04-01 19:19:39 +0200

    Rework donation/wothdrawal match

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def home
+    @adverts = Advert.where(status: Advert.statuses[:published])
+    
     if current_user
       redirect_to dashboard_index_path
     end
